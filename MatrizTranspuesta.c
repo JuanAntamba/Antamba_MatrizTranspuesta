451d299e9cc6c7cae93766259f741742d4d3547c
Juan Antamba <juan.antamba@udla.edu.ec>
2023-11-23 16:30:49 -0500

    En esta parte se crea los numeros que llenaran la matriz A, estos siendo aleatorios, esto mediante un bucle for y otro for anidado y respetando los parametros de numeros de 0 a 100

--- MatrizTranspuesta.c
+++ MatrizTranspuesta.c
@@ -6,10 +6,16 @@ int main() {
     srand(time(NULL));
 
     int filas, columnas;
-    printf("Ingrese el número de filas: ");
+    printf("Ingrese el numero de filas: ");
     scanf("%d", &filas);
-    printf("Ingrese el número de columnas: ");
+    printf("Ingrese el numero de columnas: ");
     scanf("%d", &columnas);
 
     int A[filas][columnas];
+
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            A[i][j] = rand() % (100+ 1);
+        }
+    }
 }
