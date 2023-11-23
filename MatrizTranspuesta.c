52144bb94c2def7154540738eb04f0da6eb549f4
Juan Antamba <juan.antamba@udla.edu.ec>
2023-11-23 16:24:17 -0500

    Se pide al usuario el numero de filas y columnas, declarando las filas y columnas para declarar el tamaño de la primera matriz

--- MatrizTranspuesta.c
+++ MatrizTranspuesta.c
@@ -4,5 +4,12 @@
 
 int main() {
     srand(time(NULL));
-}
 
+    int filas, columnas;
+    printf("Ingrese el número de filas: ");
+    scanf("%d", &filas);
+    printf("Ingrese el número de columnas: ");
+    scanf("%d", &columnas);
+
+    int A[filas][columnas];
+}
